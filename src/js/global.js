d0abec2395f8ab7bfbbb5986a0086bbb4377f148
gtasa3box <gtasa3box@gmail.com>
2021-09-15 16:28:25 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -1,81 +1,10 @@
 //product block - navigate
-$('.navigate__elem_1').on('click', function(){
-    $('.navigate__elem').removeClass('active');
-    $(this).addClass('active');
-    $('.product__elem').removeClass('active');
-    $('.product__elem_1').addClass('active');
-  });
-
-$('.navigate__elem_2').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_2').addClass('active');
+$('.navigate__elem').on('click', function(){
+  $('.navigate__elem').removeClass('active')
+  $(this).addClass('active')
+  $('.product__elem').removeClass('active')
+  $($(this).data('target')).addClass('active')
 });
-
-$('.navigate__elem_3').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_3').addClass('active');
-});
-
-$('.navigate__elem_4').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_4').addClass('active');
-});
-
-$('.navigate__elem_5').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_5').addClass('active');
-});
-
-$('.navigate__elem_6').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_6').addClass('active');
-});
-
-$('.navigate__elem_7').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_7').addClass('active');
-});
-
-$('.navigate__elem_8').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_8').addClass('active');
-});
-
-$('.navigate__elem_9').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_9').addClass('active');
-});
-
-$('.navigate__elem_10').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_10').addClass('active');
-});
-
-$('.navigate__elem_11').on('click', function(){
-  $('.navigate__elem').removeClass('active');
-  $(this).addClass('active');
-  $('.product__elem').removeClass('active');
-  $('.product__elem_11').addClass('active');
-});
-
 //product block - navigate - end
     
   
