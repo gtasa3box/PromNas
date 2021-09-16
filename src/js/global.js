a2bc7b5116482722c59f3f43bef3a9577268a29e
gtasa3box <gtasa3box@gmail.com>
2021-09-16 16:50:09 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -8,17 +8,32 @@ $('.navigate__elem').on('click', function(){
 //product block - navigate - end
 
 //title-info
-// $('.title__button-hide').on('click', function(){
-//   $(this).toggleClass('active')
-//   $($(this).data('target')).toggleClass('active')
-// });
-
 $('.title__button-hide').on('click', function(){
   $(this).toggleClass('active')
   $($(this).data('target')).stop(true, false).slideToggle()
   
 });
-
 //title-info-end
+
+//select
+$(document).ready(function(){
+  $('.select-sort').select2({
+    language: "ru",
+    minimumResultsForSearch: -1,
+    width: '220px',
+    hight: '52px'
+  });
+});
+//select-end
+
+//view
+$('.view-change').on('click', function(){
+  $('.view-change').removeClass('active')
+  $(this).addClass('active')
+  // $('.product__elem').removeClass('active')
+  // $($(this).data('target')).addClass('active')
+});
+//view- navigate - end
+
     
   
